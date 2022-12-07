edf3030b082be065e5c005487961c31526fcefbb
Staluks <janchukssss@inbox.lv>
2022-12-07 11:21:30 +0200

    completed task 3.4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        byte nine = 9;
+        char a = 97;
+        byte five = 5;
+        char b = 98;
+        byte two = 2;
+        byte seven = 7;
+        byte four = 4;
+
+        System.out.println("("+nine+a+"*"+a+"-"+five+b+"+"+two+"+"+a+"-"+seven+")*(("+a+"+"+b+"-"+four+a+b+")/"+two+")");
     }
 }
